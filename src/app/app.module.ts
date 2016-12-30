5f7b392a83aba21dc111e002374ee91b95cbabc0
mnv <mnvx@yandex.ru>
2016-12-30 10:31:09 +0300

    ng2-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { MdDialog } from 'ng2-material/components/dialog';
+import { MdDataTable } from 'ng2-material/components/data-table';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MdDialog,
+    MdDataTable
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    //Ng2MaterialModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
